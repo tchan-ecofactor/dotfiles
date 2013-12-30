cd4c99f93d812b70e596397287df9b93f96348d9
tchan <timothy.chan@ecofactor.com>
2013-12-30 14:48:28 -0800

    add vimrc, improved install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #/bin/sh
-pushd .
-cd ~
-rm .bashrc
-ln -s ~/repo/dotfiles/bashrc .bashrc
-rm .bash_profile
-ln -s ~/repo/dotfiles/bash_profile .bash_profile
-popd
-
+pushd . >& /dev/null
+EXCLUDED_FILENAMES_DELIMITED_BY_DOUBLE_UNDERSCORES="__install.sh__"
+DOTFILES=~/repo/dotfiles
+cd ${DOTFILES}
+for f in *
+do
+  FILENAME_WRAPPED_BY_DOUBLE_UNDERSCORES="__${f}__"
+  if [ "${EXCLUDED_FILENAMES_DELIMITED_BY_DOUBLE_UNDERSCORES/$FILENAME_WRAPPED_BY_DOUBLE_UNDERSCORES}" = "${EXCLUDED_FILENAMES_DELIMITED_BY_DOUBLE_UNDERSCORES}" ]; then
+  	echo Linking .${f}
+    rm -f ~/.${f}
+    ln -s ${DOTFILES}/${f} ~/.${f}
+  fi
+done
+echo Done
+popd >& /dev/null
